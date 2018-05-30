0fd387cc193fbd0bc0fbe00848485994b67894d1
이우근 <woogeun@iugeuns-MacBook-Air.local>
2018-05-30 23:18:32 +0900

    수정

--- index.js
+++ index.js
@@ -46,56 +46,6 @@ app.post('/import', uploader.single('file'), (req, res) => {
 
     ws.on('close', () => {
         console.log('on close!!!');
-        fs.readFile(path.join(__dirname, 'tmp', 'test1.zip'), "binary", (err, file) => {
-            if(err) {        
-                res.writeHead(500, {"Content-Type": "text/plain"});
-                res.write(err + "\n");
-                res.end();
-                return;
-            }
-        
-            res.writeHead(200);
-            res.write(file, "binary");
-            res.end();
-        });
+        res.sendFile(path.join(__dirname, 'tmp', 'test1.zip'));
     });
 });
-
-// app.get('/load', (req, res) => {
-//     let serializedData = [];
-//     let rs = fs.createReadStream(path.join(__dirname, 'res', 'test1.ndoc'));
-//     let ws = fs.createWriteStream(path.join(__dirname, 'tmp', 'test1.zip'));
-
-//     rs.on('data', (data) => {
-//         const magicPos = data[2];
-//         const magicNum = data[magicPos];
-//         data[0] = 'P'.charCodeAt();
-//         data[1] = 'K'.charCodeAt();
-//         data[2] = 0x03;
-//         data[3] = 0x04;
-
-//         for(let i = 0; i < 60; i++) {
-//             const index = i + 4;
-//             data[index] = data[index] ^ magicNum;
-//         }
-
-//         ws.write(data);
-//         ws.end();
-//     });
-
-//     ws.on('close', () => {
-//         fs.createReadStream(path.join(__dirname, 'tmp', 'test1.zip'))
-//           .pipe(unzip.Extract({path: path.join(__dirname, 'tmp')})).on('close', () => {
-//             fs.createReadStream(path.join(__dirname, 'tmp', 'document.word.pb'), {start: 16})
-//               .pipe(zlib.createUnzip())
-//               .on('data', (data) => {
-//                 for (let i = 0, len = data.length; i < len; i++) {
-//                     serializedData.push(data[i] & 0xFF);
-//                 }
-//             }).on('close', () => {
-//                 res.json({serializedData: serializedData});
-//                 res.end();
-//             });
-//         });
-//     });
-// });
